0e716ff581d4f10d32dbd23b2afad8ce76b367d0
Taís Christofani <taiskc@gmail.com>
2022-06-24 15:17:42 -0300

    bootstrap funcionando

--- frontend/mrv/src/main.js
+++ frontend/mrv/src/main.js
@@ -1,4 +1,12 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
+import BootstrapVue from "bootstrap-vue"
 import App from './App.vue'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap-vue/dist/bootstrap-vue.css"
 
-createApp(App).mount('#app')
+Vue.use(BootstrapVue)
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
